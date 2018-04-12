a44174dff230b26fe36bcc673d450819fc3b9796
sinny777 <sinny777@gmail.com>
2018-04-12 14:59:27 +0530

    Updated entrypoint.sh..

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ elif [ "$ACTION" == "live" ]; then
      service motion stop
      echo "motion service stopped, process id $PID."
    fi
-   raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 --vflip | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$SECRET &
+   raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 --vflip | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$SECRET
    echo "Live Feed started $SECRET."
 else
    echo "Unkown parameters"

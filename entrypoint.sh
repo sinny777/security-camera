80258e5bb650098ec5bbe6401f0e07ab65b9907b
sinny777 <sinny777@gmail.com>
2018-04-12 10:40:17 +0530

    updated entrypoint.sh script...

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-motion
+if [ "$1" -eq "motion" ]; then
+   pkill raspivid
+   service motion start
+   # Check if service is running
+   service motion status
+   # Let's monitor motion.log to keep this script and container running
+   tail -F /tmp/motion/motion.log
+elif [ "$1" -eq "live" ]; then
+   service motion stop
+   raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 --vflip | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$2 &
+else
+   echo "Unkown parameters"
+fi
 
-# Start motion
-# /etc/init.d/motion restart
-
-# service motion restart
-
-# Check if service is running
-service motion status
-
-# Let's monitor motion.log to keep this script and container running
-tail -F /tmp/motion/motion.log
+# raspivid -o - -t 0 -w 1920 -h 1080 -fps 40 -b 8000000 -g 40 --vflip | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$1
